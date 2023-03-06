d2209afc142ad5d98d52b13cfc9c72ff0f750990
Astghik Simonyan <astghiksimonyan56@gmail.com>
2023-03-06 23:33:59 +0400

    feat: passing args in function sum and swapping values using destructuring assignment

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Array.concat()
+// 1. Array.concat()
 
 // Concatenate the two arrays
 // const arr1 = [1, 2, 3, 4];
@@ -9,7 +9,7 @@
 // console.log(arr3);
 
 
-// How to take arguments in sum function?
+// 2. How to take arguments in sum function?
 
 // function sum(...args) {
 //     return args.reduce((sum, current) => {
@@ -18,4 +18,23 @@
 // }
 // console.log(sum(1, 2)); // prints 3
 // console.log(sum(1, 2, 3)); // prints 6
+
+// 3. How to pass arguments in sum function?
+
+// function sum(numbers) {
+//     return numbers.reduce((sum, current)=>{
+//         return sum + current;
+//     })
+// }
+// const numbers = [1, 2, 3];
+// console.log(sum(numbers)); // prints 6
+
+// 4. Swapping Values using the Destructuring Assignment
+
+// let a = 3;
+// let b = 6;
+
+// [a, b] = [b, a];
+// console.log(a); //6
+// console.log(b); //3
 
