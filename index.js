f44f5a098f506967e21ba77bfd372e678de8a9f9
Astghik Simonyan <astghiksimonyan56@gmail.com>
2023-03-06 23:10:57 +0400

    feat: taking arguments with rest in function sum

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 // Array.concat()
-// Concatenate the two arrays
-const arr1 = [1, 2, 3, 4];
-const arr2 = [5, 6, 7, 8, 9];
-// const arr3; // [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-const arr3 = arr1.concat(arr2);
-console.log(arr3);
+// Concatenate the two arrays
+// const arr1 = [1, 2, 3, 4];
+// const arr2 = [5, 6, 7, 8, 9];
+// // const arr3; // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// const arr3 = arr1.concat(arr2);
+// console.log(arr3);
+
+
+// How to take arguments in sum function?
+
+// function sum(...args) {
+//     return args.reduce((sum, current) => {
+//     return sum + current;
+// });
+// }
+// console.log(sum(1, 2)); // prints 3
+// console.log(sum(1, 2, 3)); // prints 6
+
