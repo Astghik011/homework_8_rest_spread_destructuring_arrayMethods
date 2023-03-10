f2e720f430a0050e32ce985640a7cd78f2d40e8d
Astghik Simonyan <astghiksimonyan56@gmail.com>
2023-03-10 15:38:06 +0400

    feat: slice implementation

--- methodImplementation.js
+++ methodImplementation.js
@@ -8,3 +8,62 @@
 
 // const arr = [1,3,5,"yes", true, undefined, , " "];   // the part with empty slots isn't working
 // console.log(forEach(arr));
+
+// 2. Array.slice()
+
+// function slice(arr, start, end){
+//     const newArr = [];
+
+//         if(start && end){
+
+//             if(start < 0 && end < 0){
+//                 for(let i = start + arr.length; i > end + arr.length; i--){
+//                    newArr.unshift(arr[i]);
+//                    }  
+//                 }
+
+//             for(let i = start; i < end; i++){
+//                 newArr.push(arr[i]);
+//             }
+
+//         } else if(start){
+
+//             for(let i = start; i < arr.length; i++){
+//                 newArr.push(arr[i]);
+//             }
+
+//         } else{
+
+//             for(let i = 0; i < arr.length; i++){
+//                 newArr.push(arr[i]);
+//             }
+
+//         }
+        
+//     return newArr;
+// }
+
+// const arr = [1, 2, 35, , undefined, 56, 9, 199, 3];
+// const start = -2;
+// const end = -5;
+// console.log(slice(arr, start, end));
+// console.log(slice(arr, start));
+// console.log(slice(arr));                    //same problem here, the part with empty slots isn't working, returns undefined, 
+                                            // instead of an empty slot
+
+// 3. Array.splice()
+
+// function splice(arr, start, deleteCount, items){
+//     let newArr = [];
+//     let newStart = start + deleteCount;
+//     let counter = 0;
+    
+
+    
+// const arr = [1, 2, 3, 4, 3, 4, 7, 8, 9];
+// const start = 4;
+// const deleteCount = 2;
+// const items = [5, 6];
+// console.log(splice(arr, start, deleteCount, items));
+// console.log(splice(arr, start, deleteCount));
+
